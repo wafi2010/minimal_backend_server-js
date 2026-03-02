06bd262b3d99997e4ec8d9c0e7bdb2bd86cde74c
WAFI <wafi05@lamine-JR.Dlink>
2026-03-02 22:24:40 +0600

    Project Uploaded

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+     res.send("Hello, World!")
+})
+
+app.get("/about", (req, res) => {
+     res.send("Hello, This is about page!")
+})
+
+app.listen(3000, () => {
+     console.log('server is running...')
+})
